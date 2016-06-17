43eaa0c809f1b0938c0ac08d4ad0cbbd7c00b052
alaskanshade <alaskanshade@gmail.com>
2016-06-16 22:33:38 -0800

    Add OctoPress structure.  No content yet

---
+++ octopress/config.rb
@@ -0,0 +1,19 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/DGManager/"
+http_images_path = "/DGManager/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/DGManager/fonts"
+css_dir = "public/DGManager/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
